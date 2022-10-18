52777cd6a282a2e1d9063c76ad604b1099c4732c
Victor Costa <littledeveloperman@gmail.com>
2022-10-18 03:17:48 -0300

    Documentação completa com Storybook

--- Ignite Lab/react-design-system/tailwind.config.cjs
+++ Ignite Lab/react-design-system/tailwind.config.cjs
@@ -8,20 +8,29 @@ module.exports = {
       xs: 14,
       sm: 16,
       md: 18,
-      : 20,
-      : 24,
-      : 32,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
     },
 
     colors: {
-      'gray-900': '#121214',
-      'gray-800': '#202024',
-      'gray-400': '#7C7C8A',
-      'gray-200': '#C4C4CC',
-      'gray-100': '#E1E1E6',
+      transparent: 'transparent',
 
-      'cyan-500': '#81D8F7',
-      'cyan-300': '#9BE1FB',
+      black: '#000',
+      white: '#FFF',
+
+      gray: {
+        900: '#121214',
+        800: '#202024',
+        400: '#7C7C8A',
+        200: '#C4C4CC',
+        100: '#E1E1E6',
+      },
+
+      cyan: {
+        500: '#81D8F7',
+        300: '#9BE1FB',
+      },
     },
     extend: {
       fontFamily: {
