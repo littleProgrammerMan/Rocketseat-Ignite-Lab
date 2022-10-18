a9148819b2bf0d05cc3e651b7900cf899d2282cc
Victor Costa <littledeveloperman@gmail.com>
2022-10-18 11:08:59 -0300

    Manutencao

--- storybook-static/assets/preview.be8c9d0f.js
+++
@@ -1,2 +0,0 @@
-import{t as o}from"./index.be4d6aef.js";import"./iframe.b188bcbe.js";import"./es.map.constructor.5a0f049e.js";import"./es.number.to-fixed.e240ec6c.js";import"./index.3e0426ce.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
-//# sourceMappingURL=preview.be8c9d0f.js.map
